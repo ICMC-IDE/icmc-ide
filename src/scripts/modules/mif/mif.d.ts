b0387ea23c3e4aa8af8331afa21c5574ce2c31ec
ViniciusGiroto <viniciusgiroto@outlook.com>
2024-06-08 21:36:38 -0300

    Fixing gitignore

---
+++ src/scripts/modules/mif/mif.d.ts
@@ -0,0 +1,44 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {string} input
+* @returns {Uint8Array | undefined}
+*/
+export function parseMif(input: string): Uint8Array | undefined;
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly parseMif: (a: number, b: number, c: number) => void;
+  readonly __wbindgen_export_0: WebAssembly.Table;
+  readonly __wbindgen_add_to_stack_pointer: (a: number) => number;
+  readonly memory: WebAssembly.Memory;
+  readonly __wbindgen_malloc: (a: number, b: number) => number;
+  readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+  readonly __wbindgen_free: (a: number, b: number, c: number) => void;
+  readonly __wbindgen_thread_destroy: (a?: number, b?: number) => void;
+  readonly __wbindgen_start: () => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+/**
+* Instantiates the given `module`, which can either be bytes or
+* a precompiled `WebAssembly.Module`.
+*
+* @param {SyncInitInput} module
+* @param {WebAssembly.Memory} maybe_memory
+*
+* @returns {InitOutput}
+*/
+export function initSync(module: SyncInitInput, maybe_memory?: WebAssembly.Memory): InitOutput;
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {InitInput | Promise<InitInput>} module_or_path
+* @param {WebAssembly.Memory} maybe_memory
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function __wbg_init (module_or_path?: InitInput | Promise<InitInput>, maybe_memory?: WebAssembly.Memory): Promise<InitOutput>;
---
+++ src/scripts/modules/mif/mif_bg.wasm.d.ts
@@ -0,0 +1,11 @@
+/* tslint:disable */
+/* eslint-disable */
+export function parseMif(a: number, b: number, c: number): void;
+export const __wbindgen_export_0: WebAssembly.Table;
+export function __wbindgen_add_to_stack_pointer(a: number): number;
+export const memory: WebAssembly.Memory;
+export function __wbindgen_malloc(a: number, b: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
+export function __wbindgen_free(a: number, b: number, c: number): void;
+export function __wbindgen_thread_destroy(a: number, b: number): void;
+export function __wbindgen_start(): void;
