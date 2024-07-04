863120a7bd0a77312bcd640e27f0414d10e26999
ViniciusGiroto <viniciusgiroto@outlook.com>
2024-07-04 20:41:08 -0300

    Implementation of the delete and files methods for the filesystem API

--- src/scripts/fs.ts
+++ src/scripts/fs.ts
@@ -6,4 +6,15 @@ export default class Fs {
   write(path: string, content: string): void {
     localStorage.setItem("file:" + path, content);
   }
+
+  delete(path: string): void {
+    localStorage.removeItem("file:" + path);
+  }
+
+  files(): string[] {
+    return Array
+      .from(localStorage, (_, i) => localStorage.key(i))
+      .filter((key) => (typeof key === "string"))
+      .filter((key) => key.startsWith("file:"));
+  }
 };
