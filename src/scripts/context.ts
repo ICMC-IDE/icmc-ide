c05f870873bde1fe519de12575278410bd165039
Thitav <thiptav@gmail.com>
2024-08-19 23:16:08 -0300

    Implemented context menu, refactored file system

---
+++ src/scripts/context.ts
@@ -0,0 +1,75 @@
+const contextMenusMap = {
+  file: [
+    {
+      text: "Open",
+      event: "fileOpen",
+    },
+    {
+      text: "Rename",
+      event: "fileRenameStart",
+    },
+    {
+      text: "Delete",
+      event: "fileDelete",
+    },
+  ],
+  filepicker: [
+    {
+      text: "New File...",
+      event: "fileNew",
+    },
+    {
+      text: "New Folder...",
+      event: "folderNew",
+    },
+  ],
+};
+
+let openMenu: HTMLElement | null = null;
+
+export function contextSetup() {
+  document.addEventListener("contextmenu", (event) => {
+    event.preventDefault();
+
+    openMenu?.remove();
+    openMenu = null;
+
+    let target = event.target as HTMLElement;
+    while (!target.dataset.contextMenuId) {
+      target = target.parentElement!;
+      if (!target) {
+        return;
+      }
+    }
+
+    const menu = document.createElement("div");
+
+    for (const item of contextMenusMap[
+      target.dataset.contextMenuId as keyof typeof contextMenusMap
+    ]) {
+      const button = document.createElement("button");
+      button.innerText = item.text;
+      button.addEventListener("click", () => {
+        target.dispatchEvent(
+          new CustomEvent(item.event, {
+            detail: target.dataset.contextMenuData,
+            bubbles: true,
+          }),
+        );
+        menu.remove();
+      });
+      menu.append(button);
+    }
+
+    menu.classList.add("context-menu");
+    menu.style.transform = `translate(${event.clientX}px, ${event.clientY}px)`;
+
+    document.body.append(menu);
+    openMenu = menu;
+  });
+
+  document.addEventListener("click", () => {
+    openMenu?.remove();
+    openMenu = null;
+  });
+}
